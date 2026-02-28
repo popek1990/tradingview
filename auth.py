a7593ee4dc4ddf12513ec3cae2b1c3dbcca29347
popek1990.eth <popek1990@users.noreply.github.com>
2026-02-28 12:24:40 +0000

    fix: remove incompatible streamlit cache argument

--- auth.py
+++ auth.py
@@ -16,7 +16,7 @@ COOKIE_NAME = "tv_bot_session"
 SESSION_TTL_DAYS = 1
 
 
-@st.cache_resource(experimental_allow_widgets=True)
+@st.cache_resource
 def get_manager():
     """Returns cookie manager instance."""
     return stx.CookieManager()
